c69b3e0db1ae33eb3043c7af9444f0186eaf4d67
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-02-19 17:20:43 -0600

    resetting Journal4 adding Journal5

--- Journal5/Journal5.py
+++ Journal5/Journal5.py
@@ -23,12 +23,13 @@
 # 02/17/2019  Evan Kivolowitz      1     Created prototype of project.
 #
 ######################################################################
-import argparse, textwrap
+import argparse
+import textwrap
 import os
 import constants
 import sys
 import datetime
-
+import json
 def getDate():
     curr = datetime.date.today()
     return curr.strftime("%m/%d/%Y")
@@ -40,6 +41,8 @@ def handlePython(args):
     program_name = "main.py"
     if args.file:
         program_name = args.file
+    if args.dated:
+        program_name = args.project + ".py"
     author = "Author"
     if args.author:
         author = args.author
@@ -80,12 +83,31 @@ if __name__ == "__main__":
     parser.add_argument("project")
     parser.add_argument("language")
     parser.add_argument("--flask", action="store_true", help=constants.FLASK_HELP)
+    parser.add_argument("--dated", action="store_true", help=constants.DATES)
     parser.add_argument("--file", help=constants.SUPPORTED_FILE_HELP)
     parser.add_argument("--purpose", help=constants.PURPOSE_HELP)
     parser.add_argument("--author", help=constants.AUTHOR_HELP)
     parser.add_argument("--URL", help=constants.URL_HELP)
     
     args = parser.parse_args()
+    if args.dated:
+        with open("dates.json", "r") as f:
+            data = json.load(f)
+            now = getDate()
+            currDate = datetime.datetime.strptime(now, "%m/%d/%Y")
+            journalName = "default"
+            previous = None
+            for i,date in enumerate(data.keys()):
+                print(date)
+                loadedDate = datetime.datetime.strptime(date, "%m/%d/%Y")
+                if currDate < loadedDate:
+                    if i > 0:
+                        journalName = data[previous]
+                    else:
+                        journalName = data[date] # should be 0
+                    break
+                previous = date
+            args.project = journalName
 
     if args.language not in ["python", "HTML", "C"]:
         print(constants.ERR_LANGAUGE)
