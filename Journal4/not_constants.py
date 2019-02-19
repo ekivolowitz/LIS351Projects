d212f88b4d7c656e1c232552ddaab10e48e0f575
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-02-19 17:10:24 -0600

    temp commit to merge old stuff

---
+++ Journal4/not_constants.py
@@ -0,0 +1,105 @@
+README = """
+# {}
+### URL
+The URL for the source code of this project is available here: {}
+### Assignment Writeup
+#### Intentions
+
+#### Input Work Done
+
+#### Output Results
+
+#### Approach
+
+#### Thoughts on the results
+
+### Dependencies
+{}
+
+### Usage
+{}
+
+### References
+
+"""
+
+FLASK_TEMPLATE = """
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+"""
+
+FILE_HEADER = """
+######################################################################
+# Project           : {}
+#
+# Program name      : {}
+#
+# Author            : {}
+#
+# Date created      : {}
+#
+# Purpose           : {}
+#
+# Credit            :
+#
+# Use                                    Source
+#
+# Docstring template                     https://www.phusewiki.org/wiki/index.php?title=Program_Header
+# Template creation                      https://github.com/ekivolowitz/LIS351Projects/tree/master/Journal4/ 
+#
+# Revision History  :
+#
+# Date        Author              Ref    Revision 
+# {}  {}      1     Created prototype of project.
+#
+######################################################################
+"""
+
+SUPPORTED_FILE_HELP = """
+Main file that will be created. Note that only Python is currently supported.\n
+
+Default:
+*.py    :        <project>.py
+*.html  :        <project>.html
+                 <project>.css
+                 <project>.js
+*.c     :        <project>.c 
+"""
+
+FLASK_HELP = "Is this project using flask? Only works when language is python."
+
+PURPOSE_HELP = """
+Purpose of program that will be placed in --file
+
+Default:
+
+    Project created without default. Will decide what to put here later.
+    
+"""
+AUTHOR_HELP = """
+    Author of program.
+
+    Default:
+
+        Author
+"""
+
+URL_HELP = "URL for the project's github or other repository."
+
+ERR_LANGAUGE = "ERR: programming language must be 'python', 'HTML', 'C'"
+ERR_PROJECT_EXISTS = "ERR: project directory {} already exists."
+
+DEPENDENCY_PYTHON = "* [python3](https://www.python.org/download/releases/3.0/)\n"
+DEPENDENCY_FLASK = "* [Flask](http://flask.pocoo.org/)\n"
+USAGE_PYTHON = """
+```bash
+$ python3 {}
+```
+"""
+USAGE_FLASK = """
+Now navigate to `localhost:5000` in your favorite web browser!
+"""
---
+++ Journal4/not_dated.py
@@ -0,0 +1,109 @@
+
+######################################################################
+# Project           : Journal4
+#
+# Program name      : create.py
+#
+# Author            : Evan Kivolowitz
+#
+# Date created      : 02/17/2019
+#
+# Purpose           : Project to create other LIS351 journal templates.
+#
+# Credit            :
+#
+# Use                                    Source
+#
+# Docstring template                     https://www.phusewiki.org/wiki/index.php?title=Program_Header
+# Template creation                      https://github.com/ekivolowitz/LIS351Projects/tree/master/Journal4/ 
+#
+# Revision History  :
+#
+# Date        Author              Ref    Revision 
+# 02/17/2019  Evan Kivolowitz      1     Created prototype of project.
+#
+######################################################################
+import argparse, textwrap
+import os
+import constants
+import sys
+import datetime
+
+def getDate():
+    curr = datetime.date.today()
+    return curr.strftime("%m/%d/%Y")
+
+def handleC(args):
+    pass
+def handlePython(args):
+    project = args.project
+    program_name = "main.py"
+    if args.file:
+        program_name = args.file
+    author = "Author"
+    if args.author:
+        author = args.author
+    date_created = getDate()
+    purpose = ""
+    if args.purpose:
+        purpose = args.purpose
+    
+    output = constants.FILE_HEADER.format(project, program_name, author, date_created, purpose, date_created, author)
+
+    if args.flask:
+        output += constants.FLASK_TEMPLATE
+    with open("{}/{}".format(project, program_name), 'w') as f:
+        f.write(output)
+    
+
+def handleHTML(args):
+    pass
+def createREADME(args):
+    project_name = args.project
+    URL = ""
+    if args.URL:
+        URL = args.URL
+    dependency = ""
+    usage = ""
+    if args.language == "python":
+        dependency += constants.DEPENDENCY_PYTHON
+        usage += constants.USAGE_PYTHON
+        if args.flask:
+            dependency += constants.DEPENDENCY_FLASK
+            usage += constants.USAGE_FLASK
+    with open("{}/{}".format(args.project, "README.md"), 'w') as f:
+        f.write(constants.README.format(project_name, URL, dependency, usage))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("project")
+    parser.add_argument("language")
+    parser.add_argument("--flask", action="store_true", help=constants.FLASK_HELP)
+    parser.add_argument("--file", help=constants.SUPPORTED_FILE_HELP)
+    parser.add_argument("--purpose", help=constants.PURPOSE_HELP)
+    parser.add_argument("--author", help=constants.AUTHOR_HELP)
+    parser.add_argument("--URL", help=constants.URL_HELP)
+    
+    args = parser.parse_args()
+
+    if args.language not in ["python", "HTML", "C"]:
+        print(constants.ERR_LANGAUGE)
+        sys.exit(1)
+    if os.path.exists(args.project):
+        print(constants.ERR_PROJECT_EXISTS.format(args.project))
+        sys.exit(1)
+
+    # Create directory
+    os.mkdir(args.project)
+    createREADME(args)
+    if args.language == "python":
+        handlePython(args)
+    elif args.language == "HTML":
+        pass
+    elif args.language == "C":
+        pass
+    else:
+        print(constants.ERR_LANGAUGE)
+        sys.exit(1)
+
