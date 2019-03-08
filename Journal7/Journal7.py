a926410e5b0ae5f8c14a63df30eb5c2b0703deac
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-03-07 21:53:46 -0600

    Finished project

--- Journal7/Journal7.py
+++ Journal7/Journal7.py
@@ -31,6 +31,7 @@ from pprint import pprint
 import argparse
 import sys
 from termcolor import colored
+from bs4 import BeautifulSoup
 
 client = MongoClient('localhost', 27017)
 db = client.lis
@@ -74,9 +75,53 @@ def selectCourse(courses):
     print("You have selected course: {}".format(str(courses[correct_index]['name'])))
     return courses[correct_index]
 
+def getAssignments(course_id):
+    return query(constants.ASSIGNMENT_URL.format(course_id))
+
+def buildReturnLine(line, search_term):
+    if search_term in line:
+        print("{} is in \"{}\"".format(search_term, line))
+        data = ""
+        for word in line.split(" "):
+            if word != search_term:
+                data += " "
+            else:
+                data += colored(word + " ", UPDATE)
+        return data
+    return None
+def handleInsert(search_term, course):
+    course_id = course['id']
+    assignments = getAssignments(course_id)
+    if assignments is None:
+        print("ERROR: Assignments is None")
+        return
+    assignments = assignments.json()
+    results = []
+
+    for assignment in assignments:
+        try:
+            soup = BeautifulSoup(assignment['description'], 'html.parser')
+        except:
+            continue
+        text = soup.get_text().split("\n")
+        links = soup.find_all("a")
+
+        printed = False
+
+        for line in text:
+            if search_term in line:
+                if not printed:
+                    print(colored("Found {} in {}. Here are all the words that match ".format(search_term, assignment['name']), 'green'))
+                    printed = True
+                for word in line.split(" "):
+                    if word == search_term:
+                        print(colored(word, UPDATE), end=" ")
+                    else:
+                        print(word, end=" ")
+                print()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("--view", action="store_true", help=constants.VIEW_HELP)
+    parser.add_argument("search_term", help=constants.VIEW_HELP)
     args = parser.parse_args()
 
     courses = getCourses()
@@ -84,12 +129,4 @@ if __name__ == "__main__":
         sys.exit(1)
     course = selectCourse(courses)
 
-    if args.view:
-        pass
-    else:
-        pass
-
-    # for color in ['grey','red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']:
-    #    print(colored("This text is written in {}".format(color), color))
-
-
+    handleInsert(args.search_term, course)
--- Journal7/constants.py
+++ Journal7/constants.py
@@ -1,6 +1,6 @@
 MAIN_URL = "https://canvas.instructure.com"
 COURSES_URL = MAIN_URL + "/api/v1/courses"
-MODULES_URL = COURSES_URL + "/{}/modules" # course id goes in the brackets
+ASSIGNMENT_URL = COURSES_URL + "/{}/assignments?per_page=100" # course id goes in the brackets
 PROJECT_NAME = "Journal{}"
 VIEW_HELP = '''
 Specify the --view flag if you would like to look in your database, 
