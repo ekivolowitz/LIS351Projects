0bb853a10e2cbc099bc904009d382964d64ab7c9
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-05-07 01:43:24 -0500

    Added querying in block table

--- Final/main.py
+++ Final/main.py
@@ -129,6 +129,31 @@ def handleAccount(account):
                     cachedTo.append(txEntry)    
     return render_template("account.html", value=accountData, transactions=txTableData, cachedFrom=cachedFrom, cachedTo=cachedTo)
 
+@app.route("/getData", methods=['POST'])
+def queryStuff():
+    pprint(request.form)
+    f1 = "BlockNo"
+    f2 = "BlockNo"
+    con = request.form['con']
+    op1 = request.form['op1']
+    op2 = request.form['op2']
+    v1 = request.form['v1']
+    v2 = request.form['v2']
+    with sqlite3.connect("./app.db") as conn:
+        if request.form['f1'] == "Index in Block":
+            f1 = "TransactionIndex"
+        elif request.form['f1'] == "Amount":
+            f1 = "TransactionValue"
+        if request.form['f2'] == "Index in Block":
+            f2 = "TransactionIndex"
+        elif request.form['f2'] == "Amount":
+            f2 = "TransactionValue"
+        curr = conn.cursor()
+        curr.execute("SELECT * FROM BLOCKS WHERE {} {} ? {} {} {} ?".format(f1, op1, con, f2, op2), (v1,v2))
+        result = curr.fetchall()
+        return str(result)
+    return "Hello!"
+
     
 @app.route("/block/<int:number>")
 def handleBlock(number):
@@ -140,6 +165,19 @@ def handleBlock(number):
     transactions = {}
     for tx in blockData['transactions']:
         transactions[tx['hash']] = float.fromhex(tx['value']) / EthApi.WEI_ETH_CONVERSION
+
+    with sqlite3.connect("./app.db") as con:
+        curr = con.cursor()
+        curr.execute("SELECT count(*) FROM BLOCKS WHERE BlockNo = (?)", (number, ))
+        result = curr.fetchall()
+        if int(result[0][0]) == 0:
+            print("esult == 0")
+            for elem in blockData['transactions']:
+                con.execute("INSERT INTO BLOCKS (BlockNo, TransactionHash, TransactionIndex, TransactionValue) values \
+                    (?, ?, ?, ?)", (number, elem['hash'], float.fromhex(elem['transactionIndex']), float.fromhex(elem['value']) / EthApi.WEI_ETH_CONVERSION))
+            con.commit()
+        else:
+            pprint(result)
     return render_template("block.html", miner=miner, hash=hash, gas=gasUsed,transactions=transactions)
 @app.route("/transaction/<hash>")
 def handleTransaction(hash):
